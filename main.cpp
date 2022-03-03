1e2909203f93b9b473cf5ee11879668e74795535
RebeccaVonRuden <rebecca@vonruden.info>
2022-03-03 11:09:47 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include "catch.hpp"
 #include <iostream>
+#include <string>
 
 using namespace std;
 int main ()
 {
     for(int i = 1; i <= 100; i++)
     {
-        cout<<"test";       
+        cout<<"test"<< endl;       
     }
+    
+    for (int o = 1; o < 3; o++)
+    {
+        string test1 = to_string(o);
+        cout << test1 << endl;
+    }
+    
     return 0;
 }
