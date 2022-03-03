573b375e2527b9a241e2138f6aa2c88f2f91c5ce
RebeccaVonRuden <rebecca@vonruden.info>
2022-03-03 11:19:31 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,16 @@
 using namespace std;
 int main ()
 {
-    for(int i = 1; i <= 100; i++)
-    {
-        cout<<"test"<< endl;       
-    }
     
     for (int o = 1; o < 3; o++)
     {
         string test1 = to_string(o);
         cout << test1 << endl;
+    }
+    int input = 3;
+    if(input == 3)
+    {
+        cout << "Fizz" << endl;
     }
     
     return 0;
